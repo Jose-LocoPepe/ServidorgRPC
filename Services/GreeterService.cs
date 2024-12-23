2456f7caa0ea8965a9ca9327cf80c03e6fa5e88b
Jose Bautista <jose.bautista@alumnos.ucn.cl>
2024-12-23 01:46:00 -0300

    (fix) proto fix

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -5,17 +5,12 @@ namespace ServidorgRPC.Services;
 
 public class GreeterService : Greeter.GreeterBase
 {
-    private readonly ILogger<GreeterService> _logger;
-    public GreeterService(ILogger<GreeterService> logger)
+    public override Task<AgregarReply> AgregarCliente(AgregarRequest request, ServerCallContext context)
     {
-        _logger = logger;
-    }
-
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
-    }
+         // Aquí se puede agregar la lógica para agregar el cliente a la base de datos
+         return Task.FromResult(new AgregarReply
+         {
+             Message = "Cliente added successfully!"
+         });
+     }
 }
